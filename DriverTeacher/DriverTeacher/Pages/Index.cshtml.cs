29f19d350402df1192c7c4ad7dcb4a90b035d6bf
Ilya Balashov <ibalashov24@gmail.com>
2020-10-06 03:40:27 +0300

    Initial commit

---
+++ DriverTeacher/DriverTeacher/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace DriverTeacher.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string UserName { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(string username)
+        {
+            var lowerUsername = username.ToLower();
+            if (lowerUsername == string.Empty)
+            {
+                return;
+            }
+
+            UserName = lowerUsername;
+        }
+    }
+}
